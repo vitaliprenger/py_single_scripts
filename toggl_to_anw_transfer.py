c53cf77ae57466e4342a5ec8012a17be7f663e24
Vitali Prenger <80702460+vitaliprenger@users.noreply.github.com>
2023-02-02 21:25:59 +0100

    added script for anw

---
+++ toggl_to_anw_transfer.py
@@ -0,0 +1,125 @@
+import config
+import logging
+from datetime import date, datetime, timedelta
+import calendar
+from base64 import b64encode
+import requests
+import pickle
+from openpyxl import load_workbook
+from openpyxl.utils import get_column_letter
+
+def get_toggl_time_entries(start_date, end_date):
+    logging.info("get toggl time entries for " + str(start_date) + " to " + str(end_date))
+    # prepare headers
+    api_auth = b64encode(bytes(config.toggl_api_token + ":api_token", 'ascii')).decode("ascii")
+    # apiAUth = b64encode(bytes(config.toggl_cred + ":api_token", 'ascii')).decode("ascii") # alternative to api_token
+    headers = { 'Authorization' : 'Basic %s' %  api_auth }
+
+
+    # request project_list
+    project_list = {}
+    response = requests.get('https://api.track.toggl.com/api/v9/me/projects', headers=headers)
+    for project in response.json():
+        if project_list.get(project["id"]) is None:
+            project_list[project["id"]] = project["name"]
+
+
+    # request time entries for start_date to end_date
+
+    time_response = requests.get('https://api.track.toggl.com/api/v9/me/time_entries?start_date=' + str(start_date) + '&end_date=' + str(end_date), headers=headers)
+
+    time_entry_list = {}
+    # json structure {'id': 2820044493, 'workspace_id': 3242752, 'project_id': 149577627, 'task_id': None, 'billable': False, 'start': '2023-01-27T13:15:34+00:00', 'stop': '2023-01-27T14:00:34Z', 'duration': 2700, 'description': 'Recherche crisp-dm | asum-dm', 'tags': None, 'tag_ids': None, 'duronly': True, 'at': '2023-01-27T13:59:06+00:00', 'server_deleted_at': None, ...}
+    for time_entry in time_response.json():
+        # skip entries with negative duration -> current running entries
+        # skip entries with $ in description -> entries with $ are were done by other persons
+        if time_entry["duration"] < 0 or "$" in time_entry["description"]:
+            continue
+        
+        if time_entry_list.get(project_list[time_entry["project_id"]]) is None:
+            time_entry_list[project_list[time_entry["project_id"]]] = {}
+            
+        date = datetime.strptime(time_entry["start"], "%Y-%m-%dT%H:%M:%S%z").date()
+        if time_entry_list[project_list[time_entry["project_id"]]].get(date) is None:
+            time_entry_list[project_list[time_entry["project_id"]]][date] = {}
+        
+        if time_entry_list[project_list[time_entry["project_id"]]][date].get("hours") is None:
+                time_entry_list[project_list[time_entry["project_id"]]][date]["hours"] = time_entry["duration"] / 3600
+        else:
+            time_entry_list[project_list[time_entry["project_id"]]][date]["hours"] += time_entry["duration"] / 3600
+                
+    # pickle.dump(time_entry_list, open("time_entry_list.pickle", "wb"))
+    return time_entry_list
+
+    
+def update_entries_in_anw (time_entry_list, file_path):
+    logging.info("update entries in anw")
+    wb = load_workbook(file_path)
+    anw = wb["ANW"]
+    for project in time_entry_list:
+        logging.debug("project: " + project)
+        
+        if "Reisen" not in project:
+            col_start = 8
+            col_end = 34
+        else:
+            col_start = 36
+            col_end = 40
+        
+        for col in range(col_start, col_end):
+            if anw.cell(row=4, column=col).value is None or anw.cell(row=4, column=col).value == "":
+                continue
+            excel_proj = anw.cell(row=4, column=col).value[:4]
+            if excel_proj == project[:4]:
+                project_col = col
+                break
+        
+        if project_col is None or excel_proj != project[:4]:
+            logging.error("project not found in excel")
+            raise Exception("project not found in excel")
+        
+        
+        for date in time_entry_list[project]:
+            logging.debug("date: " + str(date))
+            logging.debug("hours: " + str(time_entry_list[project][date]["hours"]))
+            
+            row_offset = 5
+            
+            day = date.day
+            
+            anw.cell(row = day + row_offset, column=col).value = time_entry_list[project][date]["hours"]
+                    
+                    
+    wb.save(file_path.replace(".xlsx", "") + "n.xlsx")
+
+
+if __name__ == '__main__':
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s %(levelname)s %(message)s",
+        handlers=[
+            logging.FileHandler("debug.log"),
+            logging.StreamHandler()
+        ]
+    )
+
+    logging.info("----------------------------------------")
+    logging.info("Starting Toggl to Jira Transfer")
+    logging.debug("Debugging is enabled")
+    
+    start_date = date(2023, 1, 1)
+    start_date = start_date.replace(day=1)
+    end_date = (start_date + timedelta(days=32)).replace(day=1) # start of next month
+    logging.debug("Start Date: " + str(start_date))
+    logging.debug("End Date: " + str(end_date))
+        
+    time_entry_list = get_toggl_time_entries(start_date, end_date)
+
+    # time_entry_list = pickle.load(open("time_entry_list.pickle", "rb"))
+    
+    file_path = "C:/Users/PrV/OneDrive - viadee Unternehmensberatung AG/Arbeitsnachweis/Anw_PrV_" + str(start_date.year) + str(start_date.month).zfill(2) + ".xlsx"
+    
+    update_entries_in_anw(time_entry_list, file_path)
+    
+    
--- toggl_to_jira_transfer.py
+++ toggl_to_jira_transfer.py
@@ -1,92 +1,72 @@
 import config
 import logging
 from datetime import date, datetime, timedelta
-import calendar
 from base64 import b64encode
 import requests
 import re
 import pickle
 from jira import JIRA
 
-
 def get_toggl_time_entries(start_date, end_date):
     logging.info("get toggl time entries for " + str(start_date) + " to " + str(end_date))
     # prepare headers
-    apiAUth = b64encode(bytes(config.toggl_api_token + ":api_token", 'ascii')).decode("ascii")
+    api_auth = b64encode(bytes(config.toggl_api_token + ":api_token", 'ascii')).decode("ascii")
     # apiAUth = b64encode(bytes(config.toggl_cred + ":api_token", 'ascii')).decode("ascii") # alternative to api_token
-    headers = { 'Authorization' : 'Basic %s' %  apiAUth }
+    headers = { 'Authorization' : 'Basic %s' %  api_auth }
 
 
-    # request projectList
-    projectList = {}
+    # request project_list
+    project_list = {}
     response = requests.get('https://api.track.toggl.com/api/v9/me/projects', headers=headers)
     for project in response.json():
-        if projectList.get(project["id"]) is None:
-            projectList[project["id"]] = project["name"]
+        if project_list.get(project["id"]) is None:
+            project_list[project["id"]] = project["name"]
 
 
     # request time entries for start_date to end_date
 
-    timeResponse = requests.get('https://api.track.toggl.com/api/v9/me/time_entries?start_date=' + str(start_date) + '&end_date=' + str(end_date), headers=headers)
+    time_response = requests.get('https://api.track.toggl.com/api/v9/me/time_entries?start_date=' + str(start_date) + '&end_date=' + str(end_date), headers=headers)
 
-    timeEntryList = {}
+    time_entry_list = {}
     # json structure {'id': 2820044493, 'workspace_id': 3242752, 'project_id': 149577627, 'task_id': None, 'billable': False, 'start': '2023-01-27T13:15:34+00:00', 'stop': '2023-01-27T14:00:34Z', 'duration': 2700, 'description': 'Recherche crisp-dm | asum-dm', 'tags': None, 'tag_ids': None, 'duronly': True, 'at': '2023-01-27T13:59:06+00:00', 'server_deleted_at': None, ...}
-    for timeEntry in timeResponse.json():
+    for time_entry in time_response.json():
         # skip entries with negative duration -> current running entries
-        if timeEntry["duration"] < 0:
+        if time_entry["duration"] < 0:
             continue
         
-        if timeEntryList.get(projectList[timeEntry["project_id"]]) is None:
-            timeEntryList[projectList[timeEntry["project_id"]]] = {}
+        if time_entry_list.get(project_list[time_entry["project_id"]]) is None:
+            time_entry_list[project_list[time_entry["project_id"]]] = {}
         
-        date = datetime.strptime(timeEntry["start"], "%Y-%m-%dT%H:%M:%S%z").date()
-        if timeEntryList[projectList[timeEntry["project_id"]]].get(date) is None:
-            timeEntryList[projectList[timeEntry["project_id"]]][date] = {}
+        date = datetime.strptime(time_entry["start"], "%Y-%m-%dT%H:%M:%S%z").date()
+        if time_entry_list[project_list[time_entry["project_id"]]].get(date) is None:
+            time_entry_list[project_list[time_entry["project_id"]]][date] = {}
         
         # seperate Eucon cases into Ticket-ID
-        if "2779 Produkt" in projectList[timeEntry["project_id"]]:
-            ticket = re.search(r"^\w+-\d+", timeEntry["description"], re.IGNORECASE).group(0)
-            if timeEntryList[projectList[timeEntry["project_id"]]][date].get(ticket) is None:
-                timeEntryList[projectList[timeEntry["project_id"]]][date][ticket] = {}
+        euc_ticket_string_reg = r"^\w+-\d+ - "
+        if "2779 Produkt" in project_list[time_entry["project_id"]]:
+            ticket = re.search(r"^\w+-\d+", time_entry["description"], re.IGNORECASE).group(0)
+            if time_entry_list[project_list[time_entry["project_id"]]][date].get(ticket) is None:
+                time_entry_list[project_list[time_entry["project_id"]]][date][ticket] = {}
             
-            if timeEntryList[projectList[timeEntry["project_id"]]][date][ticket].get("hours") is None:
-                timeEntryList[projectList[timeEntry["project_id"]]][date][ticket]["hours"] = timeEntry["duration"] / 3600
+            if time_entry_list[project_list[time_entry["project_id"]]][date][ticket].get("hours") is None:
+                time_entry_list[project_list[time_entry["project_id"]]][date][ticket]["hours"] = time_entry["duration"] / 3600
             else:
-                timeEntryList[projectList[timeEntry["project_id"]]][date][ticket]["hours"] += timeEntry["duration"] / 3600
+                time_entry_list[project_list[time_entry["project_id"]]][date][ticket]["hours"] += time_entry["duration"] / 3600
             
-            if timeEntryList[projectList[timeEntry["project_id"]]][date][ticket].get("description") is None:
-                description = re.sub(r"^\w+-\d+ - ", "", timeEntry["description"])
+            if time_entry_list[project_list[time_entry["project_id"]]][date][ticket].get("description") is None:
+                description = re.sub(euc_ticket_string_reg, "", time_entry["description"])
                 if "#" in description:
                     description = description[:description.index("#")].strip()
-                timeEntryList[projectList[timeEntry["project_id"]]][date][ticket]["description"] = description
+                time_entry_list[project_list[time_entry["project_id"]]][date][ticket]["description"] = description
             else:
-                description = re.sub(r"^\w+-\d+ - ", "", timeEntry["description"])
+                description = re.sub(euc_ticket_string_reg, "", time_entry["description"])
                 if "#" in description:
                     description = description[:description.index("#")].strip()
-                if description not in timeEntryList[projectList[timeEntry["project_id"]]][date][ticket]["description"]:
-                    timeEntryList[projectList[timeEntry["project_id"]]][date][ticket]["description"] += ", " + description
-        
-        else:
-            if timeEntryList[projectList[timeEntry["project_id"]]][date].get("hours") is None:
-                timeEntryList[projectList[timeEntry["project_id"]]][date]["hours"] = timeEntry["duration"] / 3600
-            else:
-                timeEntryList[projectList[timeEntry["project_id"]]][date]["hours"] += timeEntry["duration"] / 3600
-            
-            if timeEntryList[projectList[timeEntry["project_id"]]][date].get("description") is None:
-                description = re.sub(r"^\w+-\d+ - ", "", timeEntry["description"])
-                if "#" in description:
-                    description = description[:description.index("#")].strip()
-                timeEntryList[projectList[timeEntry["project_id"]]][date]["description"] = description
-            else:
-                description = re.sub(r"^\w+-\d+ - ", "", timeEntry["description"])
-                if "#" in description:
-                    description = description[:description.index("#")].strip()
-                if description not in timeEntryList[projectList[timeEntry["project_id"]]][date]["description"]:
-                    timeEntryList[projectList[timeEntry["project_id"]]][date]["description"] += ", " + description
+                if description not in time_entry_list[project_list[time_entry["project_id"]]][date][ticket]["description"]:
+                    time_entry_list[project_list[time_entry["project_id"]]][date][ticket]["description"] += ", " + description
 
-    pickle.dump(timeEntryList, open("timeEntryList.pickle", "wb"))
-    return timeEntryList
-
+    # pickle.dump(time_entry_list, open("time_entry_list.pickle", "wb"))
+    return time_entry_list
 
 def get_eucon_jira_worklog_list(start_date, end_date):
     logging.info("get eucon jira worklog for " + str(start_date) + " to " + str(end_date))
@@ -114,7 +94,7 @@ def get_eucon_jira_worklog_list(start_date, end_date):
                 if jiraWorklogList[worklogDate].get(issue.key) is None:
                     jiraWorklogList[worklogDate][issue.key] = worklog["timeSpentSeconds"] / 3600
     
-    pickle.dump(jiraWorklogList, open("jiraWorklogList.pickle", "wb"))
+    # pickle.dump(jiraWorklogList, open("jiraWorklogList.pickle", "wb"))
     return jiraWorklogList
 
 
@@ -131,11 +111,22 @@ def add_missing_entries_for_eucon (timeEntryList, jiraWorklogList):
             for date in timeEntryList[project]:
                 for ticket in timeEntryList[project][date]:
                     if jiraWorklogList.get(date) is None or jiraWorklogList[date].get(ticket) is None or jiraWorklogList[date][ticket] != timeEntryList[project][date][ticket]["hours"]:
+                        
+                        hours = timeEntryList[project][date][ticket]["hours"]
+                        desc = timeEntryList[project][date][ticket]["description"]
+                        if jiraWorklogList.get(date) is not None and jiraWorklogList[date].get(ticket) is not None and \
+                                time_entry_list[project][date][ticket]["hours"] != jiraWorklogList[date][ticket]:
+                            
+                            hours_jira = jiraWorklogList[date].get(ticket)
+                            logging.info("Wrong time in Jira. toggl: " + str(hours) + "h, Jira: " + str(hours_jira) + "h. " + ticket + " on " + str(date))
+                            answer = input("Should the entry nevertheless be added? (y/n): ")
+                            if answer != "y":
+                                continue
                         # add missing entry to Jira
-                        logging.info("Adding missing entry for " + ticket + " on " + str(date) + " with " + str(timeEntryList[project][date][ticket]["hours"]) + "h")
+                        logging.info("Adding missing entry for " + ticket + " on " + str(date) + " with " + str(hours) + "h")
                         # date to datetime with timezone
                         datetime_with_zone = datetime.strptime(date.strftime("%Y-%m-%d") + "T00:00:00.000+0100", '%Y-%m-%dT%H:%M:%S.000%z')
-                        jira.add_worklog(ticket, timeSpentSeconds=timeEntryList[project][date][ticket]["hours"] * 3600, started=datetime_with_zone, comment=timeEntryList[project][date][ticket]["description"])
+                        jira.add_worklog(ticket, timeSpentSeconds = hours * 3600, started=datetime_with_zone, comment=desc)
 
 
 
@@ -154,7 +145,7 @@ if __name__ == '__main__':
     logging.info("Starting Toggl to Jira Transfer")
     logging.debug("Debugging is enabled")
     
-    start_date = date(2023, 1, 15)
+    start_date = date(2023, 1, 1)
     end_date = datetime.now().date()
     logging.debug("Start Date: " + str(start_date))
     logging.debug("End Date: " + str(end_date))
