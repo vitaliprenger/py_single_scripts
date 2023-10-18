460c3b5d93403cc6d0f1969131e2685594210759
vitali_prenger_ext <vitali_prenger_ext@vm-dbs-ssis>
2023-10-18 16:45:13 +0200

    Bugfix project checkout

--- git_checkout_multiple_repos.py
+++ git_checkout_multiple_repos.py
@@ -1,6 +1,35 @@
 import config
 import gitlab
 import subprocess
+import os
+
+def fetch_projects(group):
+       page = 1
+       per_page = 100
+
+       while True:
+           projects = group.projects.list(per_page=per_page, page=page)
+           if not projects:
+               break
+
+           for project in projects:
+               git_url = project.http_url_to_repo
+               name_tech = project.path
+               project_path = os.path.join(path, subfolder, name_tech + '.git')
+
+               if not os.path.exists(project_path):
+                   print(f"Cloning {name_tech}...")
+                   subprocess.call(['git', 'clone', git_url, project_path])
+               else:
+                   print(f"Skipping {name_tech} as it's already cloned.")
+
+           page += 1
+
+       subgroups = group.subgroups.list()
+       for subgroup in subgroups:
+           print(f"Fetching projects from subgroup: {subgroup.name}")
+           fetch_projects(subgroup)
+
 
 if __name__ == '__main__':
    api_token = config.eucon_gitlab_api_token
@@ -23,7 +52,7 @@ if __name__ == '__main__':
    # for project in projects:
    #    print(project) 
       
-   path = 'C:/Users/vitali.prenger/repos'
+   path = 'C:/Users/vitali_prenger_ext/repos'
    print(path)
    
    # group = gl.groups.get(158) # 160 = SSIS-Pakete # 158 = Team-Data
@@ -32,16 +61,5 @@ if __name__ == '__main__':
    group = gl.groups.get(160) # 160 = SSIS-Pakete # 158 = Team-Data # 170 = SSRS-Berichte
    subfolder = group.path
    
-   page = 1
-   per_page = 100  # You can adjust this to fetch more projects per page
-   projects = gl.projects.list(group_id=group.id, per_page=per_page, page=page)
-   
-   while projects:
-      for project in projects:
-         git_url = project.http_url_to_repo
-         name_tech = project.path
-         print(path + '/' + subfolder + '/' + name_tech + '.git')
-         subprocess.call(['git', 'clone', git_url, path + '/' + subfolder + '/' + name_tech + '.git'])
-      
-      page += 1
-      projects = gl.projects.list(group_id=group.id, per_page=per_page, page=page)
+   fetch_projects(group)
+
