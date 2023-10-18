f18d15c6cf77db5e0f56ea18318e1f880a558a2b
Vitali Prenger (viadee) <vitali.prenger@eucon.com>
2023-10-18 14:03:26 +0000

    Make checkout get more than 20 Projects

--- git_checkout_multiple_repos.py
+++ git_checkout_multiple_repos.py
@@ -17,24 +17,31 @@ if __name__ == '__main__':
    # information is being sent back and forth to the GitLab server.
    # Note: this will cause credentials and other potentially sensitive
    # information to be printed to the terminal.
-   gl.enable_debug()
+   # gl.enable_debug()
    
    projects = gl.projects.list(iterator=True)  
    # for project in projects:
    #    print(project) 
       
-   path = '/mnt/c/proj/euc'
+   path = 'C:/Users/vitali.prenger/repos'
    print(path)
    
    # group = gl.groups.get(158) # 160 = SSIS-Pakete # 158 = Team-Data
    # group_childs = group.descendant_groups.list()
    
    group = gl.groups.get(160) # 160 = SSIS-Pakete # 158 = Team-Data # 170 = SSRS-Berichte
-   
    subfolder = group.path
-   grp_list = group.projects.list()
-   for project in grp_list:
-      git_url = project.http_url_to_repo
-      name_tech = project.path
-      print(path + '/' + subfolder + '/' + name_tech + '.git')
-      subprocess.call(['git', 'clone', git_url, path + '/' + subfolder + '/' + name_tech + '.git'])
+   
+   page = 1
+   per_page = 100  # You can adjust this to fetch more projects per page
+   projects = gl.projects.list(group_id=group.id, per_page=per_page, page=page)
+   
+   while projects:
+      for project in projects:
+         git_url = project.http_url_to_repo
+         name_tech = project.path
+         print(path + '/' + subfolder + '/' + name_tech + '.git')
+         subprocess.call(['git', 'clone', git_url, path + '/' + subfolder + '/' + name_tech + '.git'])
+      
+      page += 1
+      projects = gl.projects.list(group_id=group.id, per_page=per_page, page=page)
