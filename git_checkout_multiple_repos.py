b7ed2f32772be842e38320d0d0dce3df7e3b478a
Vitali Prenger <80702460+vitaliprenger@users.noreply.github.com>
2024-07-27 20:05:26 +0200

    skip archived projects in git checkout

--- git_checkout_multiple_repos.py
+++ git_checkout_multiple_repos.py
@@ -2,6 +2,7 @@ import helper.config as config
 import gitlab
 import subprocess
 import os
+import shutil  # Import shutil for deleting directories
 
 def fetch_projects(group):
     page = 1
@@ -13,10 +14,12 @@ def fetch_projects(group):
             break
 
         for project in projects:
-            if project.attributes['archived'] == False:
+            with_archived_projects = False
+            project_path = os.path.join(path, subfolder, project.path + '.git')
+            
+            if with_archived_projects or project.attributes['archived'] == False:
                 git_url = project.http_url_to_repo
                 name_tech = project.path
-                project_path = os.path.join(path, subfolder, name_tech + '.git')
 
                 if not os.path.exists(project_path):
                     print(f"Cloning {name_tech}...")
@@ -30,12 +33,15 @@ def fetch_projects(group):
                         # No branch is checked out, checkout master
                         subprocess.call(['git', 'checkout', 'master'])
                     subprocess.call(['git', 'pull'])  # Pull from current branch or master
+            elif os.path.exists(project_path): # if archived projects are excluded but the folder exists
+                print(f"Found archived project {project.path}. Deleting folder now...")
+                shutil.rmtree(project_path)  # Delete the project directory
 
         page += 1
 
     subgroups = group.subgroups.list()
     for subgroup in subgroups:
-        print(f"Fetching projects from subgroup: {subgroup.name}")
+        print(f"Fetching projects from subPgroup: {subgroup.name}")
         fetch_projects(subgroup)
 
 
@@ -49,14 +55,14 @@ if __name__ == '__main__':
    
    projects = gl.projects.list(iterator=True)  
       
-
+    # Uncomment the appropriate path based on your environment
 #    path = 'C:\\Users\\vitali_prenger\\repos' -- 
 #    path = 'C:\\Users\\vitali_prenger_ext\\repos' -- ssis vm
    path = '/mnt/c/proj/euc' # Laptop Vit
    print(path)
    
    # 160 = SSIS-Pakete # 170 = SSRS-Berichte # 158 = Team-Data  # 1513 = Databases # 179 = Datenlieferung
-   group = gl.groups.get(170) 
+   group = gl.groups.get(160) 
    subfolder = group.path
    
    fetch_projects(group)
