0b01f7e279dc6b6c833200837773b6ad20ccb03d
Vitali Prenger <vitali.prenger-extern@eucon.com>
2026-04-01 19:06:53 +0200

    refactor search_string_in_repo_files_with_hist.py for improved readability and updated search parameters

--- search_string_in_repo_files_with_hist.py
+++ search_string_in_repo_files_with_hist.py
@@ -1,19 +1,26 @@
 import os
 import subprocess
 
+
 def search_string_in_repo(repo_path, search_string):
     os.chdir(repo_path)
     subprocess.run(["git", "fetch", "--all"])
     subprocess.run(["git", "checkout", "--force", "origin/master"])
 
     # Use a text search tool like grep
-    result = subprocess.run(["grep", "-r", "-i", search_string, "."], capture_output=True, text=True)
+    result = subprocess.run(
+        ["grep", "-r", "-i", search_string, "."], capture_output=True, text=True
+    )
     print(result.stdout)
 
+
 # Example usage
-basefolder = '/mnt/c/proj/euc/ssis-pakete/'
+basefolder_wsl = "/mnt/c/proj/euc/ssrs-pakete/"
+basefolder_win = "C:\\proj\\euc\\insurance\\team-data\\ssrs-berichte"
+basefolder = basefolder_win  # Change to basefolder_wsl if running in WSL
 repositories = subfolders = [f.path for f in os.scandir(basefolder) if f.is_dir()]
-search_string = 'KL_neu'
+search_string = "ckzahlenauswertung_Gesamt"
 
 for repo in repositories:
-    search_string_in_repo(repo, search_string)
+    if repo.endswith("SSRS-OnPrem.git"):
+        search_string_in_repo(repo, search_string)
